987409ac5185cd4c71acc3f641fa7dd158c7684a
didi <ixchel_2v@hotmail.es>
2019-01-31 13:27:29 -0600

    Cambios de inicio

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,16 @@
-window.cipher = {
-  // ... 
-};
+// window.cipher = (){
+// ...
+// };
+
+
+    // cipher.encode(offset, string): offset es el número de posiciones que queremos mover a la derecha en el alfabeto y string el mensaje (texto) que queremos cifrar.
+    // cipher.decode(offset, string): offset es el número de posiciones que queremos mover a la izquierda en el alfabeto y string el mensaje (texto) que queremos descifrar.
+
+// Creando el codigo
+const Clave = ()=> {
+ const bot1 = document.getElementById('bot1').value;
+    return  console.log(bot1);
+    // Formula (x + n) % 26 ___(x-65+n)%26+65
+
+}
+//  console.log("displacear string = ABC
---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+ const abc =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+// for (let i= 0, l= abc.length; i< l; i++ ) {
+//   let displacear = abc;
+// }
