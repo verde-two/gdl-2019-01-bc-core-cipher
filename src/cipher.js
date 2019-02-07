aaa996f8c486eddd99c58af7ff7b057b30745288
didi <ixchel_2v@hotmail.es>
2019-02-06 19:00:17 -0600

    nota2

--- src/cipher.js
+++ src/cipher.js
@@ -4,13 +4,11 @@ window.cipher = {
       for(let i = 0; i<texto.length; i++){
         let caracter = texto[i];
         let ascii = caracter.charCodeAt(0);
-        console.log((ascii -65+ offset)%26+65);
         let codificar = (ascii -65+ offset)%26+65;
         let alfa = String.fromCharCode(codificar);
         palabra.push(alfa);
       }
       let palabra2 = palabra.join("");
-      console.log(palabra2);
       return palabra2;
 
 },
@@ -20,7 +18,6 @@ window.cipher = {
       for(let i = 0; i<texto.length; i++){
       let caracter = texto[i];
       let ascii = caracter.charCodeAt(0);
-      console.log((ascii +65- offset)%26+65);
       let decodificar = (ascii +65- offset)%26+65;
       let alfa = String.fromCharCode(decodificar);
       palabra.push(alfa);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 //Enligando datos par cifrar
 function mostrar(){
-   console.log(document.getElementById("offset").value);
+
    let saveOf = Number(document.getElementById("offset").value);//Guardando valor de offset
    let text1 = document.getElementById("texto").value;//Guardando valor de texto
    let run = cipher.encode(saveOf, text1);//Juntando offset y test
-   console.log(run);
    document.getElementById("cod").innerHTML = run;
 
 }
@@ -13,11 +12,9 @@ function mostrar(){
 //Enligando datos para decifrar
 function terminar(){
 
-  console.log(document.getElementById("offset").value);
   let save = document.getElementById("offset").value;//Guardando valor de offset
   let text2 = document.getElementById("texto2").value;//Guardando valor de texto2
   let run2 = cipher.decode(save, text2);//Juntando offset y test
-  console.log(run2);
   document.getElementById("cod2").innerHTML = run2;
 
 }
